6588ff0ac55f70b690b95a5eac67c601da810f7c
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-08-25 20:39:26 -0400

    updateapi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const APIURL = 'https://openweathermap.org/api/one-call-api';
-async function getWeatherbyLocation (location){
-    const resp =await fetch(APIURL+'/location/search/?query='+location);
-    const respData = await resp.json()
+const apikey = "c572c0ecfa50161e5b2d94a57854c073"; 
+const url = (location) =>
+ `https://api.openweathermap.org/data/2.5/weather?q=Detroit,us&APPID=c572c0ecfa50161e5b2d94a57854c073`;
+
+ async function getWeatherbyLocation(location) {
+    const resp = await fetch(url(location), {
+        origin:"cors"});
+    const respData = await resp.json();
 
     console.log(respData);
 }
-getWeatherbyLocation('Detroit');
+getWeatherbyLocation("Detroit");
